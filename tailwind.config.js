6ad1de958c76028494558044f1332af9540ca035
Yusuf <usuf7hahin@gmail.com>
2024-12-22 09:01:05 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        shahin: "#A0D0AD",
+      },
+      fontFamily: {
+        yushah: "Poppins",
+        random: "Playwrite Hrvatska Lijeva Guides",
+      },
+    },
+  },
+  plugins: [],
+}
